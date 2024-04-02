9e407074ff035f732cbf62743db3d9f026cd4114
MirAfzalul <mirafzalulbashar@gmail.com>
2024-04-02 01:38:24 -0400

    UI file structure comlete

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { Link } from "react-router-dom"
+import Navbar from "./assets/componenets/Navbar"
+import SideBar from "./assets/componenets/SideBar"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Navbar/>
+     <SideBar/>
+
+    </>
+  )
+}
+
+export default App
